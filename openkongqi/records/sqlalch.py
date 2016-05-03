68f6e10418a93b6b448aed1e00bb6e60b61717bb
Eddo Hintoso <eddo@measureofquality.com>
2016-05-03 12:29:16 +0800

    Reliable writing records method to DB
    First, try to avoid using loops and add all together at once.
    This is mainly for speed performance.
    
    Then, try splitting the input records into data chunks, and
    add the chunks all at once (to avoid loops once again).
    
    If there happens to be some error when trying to commit
    a chunk of records (perhaps due to duplicates), final fallback
    is to write the records of that one bulk one-by-one.
    
    This process is much slower but ensures that the records do
    get written onto the DB without any fail.

--- openkongqi/records/sqlalch.py
+++ openkongqi/records/sqlalch.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 import pytz
 
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Column, String, DateTime, Float
 from sqlalchemy.orm import scoped_session, sessionmaker, load_only
@@ -51,18 +52,36 @@ class SQLAlchemyRecordsWrapper(BaseRecordsWrapper):
 
     def write_record(self, record, commit=True):
         if not self.is_duplicate(record):
-            r = Record(ts=record[0].astimezone(pytz.utc),  # UTC timezne
-                       uuid=record[1],
-                       key=record[2],
-                       value=record[3])
+            r = Record.from_tuple(record)
             self._cnx.add(r)
-        if commit:
-            self._cnx.commit()
+            if commit:
+                self._cnx.commit()
 
-    def write_records(self, records):
-        for record in records:
-            self.write_record(record, commit=False)
-        self._cnx.commit()
+    def write_records(self, records, commit_size=100):
+        # first map through list to convert to
+        # SQLAlchemy objects to be ready to committed to DB
+        recs = map(lambda r: Record.from_tuple(r),
+                   records)
+        try:  # the fastest way
+            self._cnx.add_all(recs)
+            self._cnx.commit()
+        except sqlalchemy.exc.IntegrityError:
+            # first rollback to restart session
+            self._cnx.rollback()
+            # split records into chunks of given size
+            # the size of the last chunk is the remainder left
+            row_chunks = [recs[i:i + commit_size]
+                          for i in range(0, len(recs), commit_size)]
+            for chunk in row_chunks:
+                try:
+                    self._cnx.add_all(chunk)
+                    self._cnx.commit()
+                except sqlalchemy.exc.IntegrityError:
+                    # resort to committing one by one
+                    self._cnx.rollback()  # restart session
+                    for rec in chunk:
+                        rec = Record.to_tuple(rec)
+                        self.write_record(rec, commit=True)
 
     def get_records(self, start, end, filters=None):
         query = self._cnx.query(Record)
@@ -86,4 +105,41 @@ class Record(Base):
         return (
             "<Record(ts='{ts}', uuid='{uuid}', key='{key}', value='{value}')>"
             .format(ts=self.ts, uuid=self.uuid, key=self.key, value=self.value)
+        )
+
+    @classmethod
+    def from_tuple(self, record):
+        """Return `Record` object from a tuple.
+
+        The indices of `record` should correspond to the following criteria:
+            0 - datetime.datetime object with timezone information
+            1 - uuid key
+            2 - string indicating type of pollutant
+            3 - value of the pollutant
+
+        :returns: openkongqi.records.sqlalch.Record
+        """
+        return Record(
+            ts=record[0].astimezone(pytz.utc),  # UTC tz
+            uuid=record[1],
+            key=record[2],
+            value=record[3]
+        )
+
+    @classmethod
+    def to_tuple(self, record):
+        """Return a tuple from a `Record` object.
+
+        .. note:: Inverse function of `Record.from_tuple`
+
+        The tuple returned should have the same data structure as
+        the `record` parameter passed to `Record.from_tuple`.
+
+        :returns: tuple
+        """
+        return (
+            record.ts,
+            record.uuid,
+            record.key,
+            record.value,
         )
