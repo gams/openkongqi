3caf1cbcad30cf5a9d517391da3686b7cc1136fd
Eddo Hintoso <eddo@measureofquality.com>
2016-07-12 18:22:21 +0800

    Change data extraction format
    {
      "cn:shanghai:jinganjiancezhan": [
        {
          "ts": datetime.datetime(2016, 7, 11, 16, 31, 10, tzinfo=<...>),
          "fields": {
            "pm25": 0,
            "pm10": 0,
            ...
          },
        },
        {...}
      ],
      "cn:shanghai:hongkou": [...],
      ...,
    }

--- openkongqi/source/pm25in.py
+++ openkongqi/source/pm25in.py
@@ -38,31 +38,30 @@ class Source(HTTPSource):
     def extract(self, content):
         soup = bs4.BeautifulSoup(content)
         time = soup.find(class_="live_data_time").p.text
-        dt = datetime.strptime(time.split(u'\uff1a')[1],
+        ts = datetime.strptime(time.split(u'\uff1a')[1],
                                '%Y-%m-%d %H:%M:%S').replace(tzinfo=self._tz)
-        data = []
+        data = {}
         for row in soup.find(id='detail-data').find_all('tr')[1:]:
             cells = row.find_all('td')
             try:
                 station = self.get_station_uuid(cells[0].string)
+                data[station] = []
             except KeyError as e:
                 logger.warning("Station not found ({}); skipping ..."
                                .format(e.message))
             else:
-                data.extend([
-                    (dt, station, 'pm25',
-                        self.pythonify(cells[4].string, is_num=True)),
-                    (dt, station, 'pm10',
-                        self.pythonify(cells[5].string, is_num=True)),
-                    (dt, station, 'co',
-                        self.pythonify(cells[6].string, is_num=True)),
-                    (dt, station, 'no2',
-                        self.pythonify(cells[7].string, is_num=True)),
-                    (dt, station, 'o3_1h',
-                        self.pythonify(cells[8].string, is_num=True)),
-                    (dt, station, 'o3_8h',
-                        self.pythonify(cells[9].string, is_num=True)),
-                    (dt, station, 'so2',
-                        self.pythonify(cells[10].string, is_num=True)),
-                ])
+                data[station].append(
+                    {
+                        'ts': ts,
+                        'fields': {
+                            'pm25': self.pythonify(cells[4].string, is_num=True),
+                            'pm10': self.pythonify(cells[5].string, is_num=True),
+                            'co': self.pythonify(cells[6].string, is_num=True),
+                            'no2': self.pythonify(cells[7].string, is_num=True),
+                            'o3_1h': self.pythonify(cells[8].string, is_num=True),
+                            'o3_8h': self.pythonify(cells[9].string, is_num=True),
+                            'so2': self.pythonify(cells[10].string, is_num=True),
+                        },
+                    }
+                )
         return data
